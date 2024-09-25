6efeb7682b8b1b4835f82f416f818f770de1ba7c
dimos <dimos86m@gmail.com>
2024-09-25 15:40:24 +0300

    Remove color picker component and update default board color

--- src/view/layout.ts
+++ src/view/layout.ts
@@ -3,7 +3,6 @@ import { Me } from '../auth';
 import { Ctrl } from '../ctrl';
 import { MaybeVNodes } from '../interfaces';
 import { href } from '../routing';
-import colorpicker from './colorpicker';
 import '../../scss/_navbar.scss';
 
 export default function (ctrl: Ctrl, body: MaybeVNodes): VNode {
@@ -48,7 +47,6 @@ const renderNavBar = (ctrl: Ctrl) =>
             )
           ),
         ]),
-        h('ul.navbar-nav', [colorpicker(), ctrl.auth.me ? userNav(ctrl.auth.me) : anonNav()]),
       ]),
     ]),
   ]);
